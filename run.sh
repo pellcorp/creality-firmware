d4863cb64b43ed06d430bb526c82e4230d5135e5
Jason Pell <jason@pellcorp.com>
2026-07-26 14:59:30 +1000

    I dont need a file provided for shell

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ -f $1 ]; then
+action=$(basename $0)
+args="--rm "
+if [ "$action" = "shell" ]; then
+  args="-ti"
+  docker run -ti -v "/tmp:/out" -v "$PWD:/build" pellcorp/creality-firmware /build/actions/shell.sh
+elif [ -f "$1" ]; then
   fqn=$(realpath $1)
   originals=$(dirname $fqn)
-  action=$(basename $0)
-  args="--rm "
-  if [ "$action" = "shell" ]; then
-    args="-ti"
-  fi
+
   docker run $args -v "/tmp:/out" -v "$PWD:/build" -v "$originals:/originals" pellcorp/creality-firmware /build/actions/${action}.sh $1
 else
   echo "FATAL: File $1 not found!"
