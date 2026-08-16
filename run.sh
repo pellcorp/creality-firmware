a20ad2be559430beb5f6cad50139b18c60e5a68f
Jason Pell <jason@pellcorp.com>
2026-08-16 12:25:42 +1000

    a custom nebula pad with a simple bootstrap server (#1)

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ elif [ -f "$1" ]; then
   fqn=$(realpath $1)
   originals=$(dirname $fqn)
 
-  docker run $args -v "/tmp:/out" -v "$PWD:/build" -v "$originals:/originals" pellcorp/creality-firmware /build/actions/${action}.sh $1
+  docker run $args -v "/tmp:/out" -v "$PWD:/build" -v "$originals:/originals" pellcorp/creality-firmware /build/actions/${action}.sh $1 $2
 else
   echo "FATAL: File $1 not found!"
   exit 1
